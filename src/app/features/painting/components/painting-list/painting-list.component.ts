4a5f33d76a0d47b5170f80d93de22563be6ce84e
Pooja Singh Jurel <pooja@zool.in>
2021-02-16 14:10:46 +0530

    add background image and create table

--- src/app/features/painting/components/painting-list/painting-list.component.ts
+++ src/app/features/painting/components/painting-list/painting-list.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaintingListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    
   }
 
 }
